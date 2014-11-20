8a7313bc1c32efb8ddd808cc17081e3f5e2f429e
Francisco Madrid <pmadrid@stratio.com>
2014-11-20 15:23:14 +0100

    Changed OS, installation method, initial message, repositories...

--- Vagrantfile
+++ Vagrantfile
@@ -8,37 +8,34 @@ VAGRANTFILE_API_VERSION = "2"
 require 'yaml'
 
 if File.exist?("vagrant_settings.yml")
-	settings = YAML::load_file("vagrant_settings.yml")
-else                                                                                                        
-    puts "Hey, required file vagrant_settings.yml is missing. Please download it here https://github.com/Stratio/sandbox"                                                                   
-    exit                                                                                                      
-end              
+   settings = YAML::load_file("vagrant_settings.yml")
+else
+   puts "Hey, required file vagrant_settings.yml is missing. Please download it here https://github.com/Stratio/sandbox"
+   exit
+end
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.vm.box = settings['sandbox_box']
+  config.vm.box = settings["sandbox_box"]
 
   config.vm.hostname="#{settings['stratio_module_name']}.#{settings['sandbox_hostname_suffix']}"
 
-
   # Create a private network, which allows host-only access to the machine
-  # using a specific IP.
-  config.vm.network "private_network", ip: settings['sandbox_network_ip']
+  config.vm.network "private_network", type: "dhcp"
 
   config.vm.post_up_message = "Stratian: your Stratio Sandbox is now up & ready..."
 
   config.vm.provider "virtualbox" do |vb|
-    # Don't boot with headless mode
+    # Boot with headless mode
     vb.gui = false
     vb.name = "stratio_sandbox_#{settings['stratio_module_name']}"
+
     # Use VBoxManage to customize the VM. For example to change memory:
-    vb.customize ["modifyvm", :id, "--memory", settings['sandbox_memory']]
-    vb.customize ["modifyvm", :id, "--cpus", settings['sandbox_cpus']]
+    vb.memory = settings['sandbox_memory']
+    vb.cpus = settings['sandbox_cpus']
     vb.customize ["modifyvm", :id, "--cpuexecutioncap", settings['sandbox_max_cpu_usage']]
   end
 
-  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
-
-  config.vm.provision :shell, :inline => "STRATIO_ENV=#{settings['stratio_env']} STRATIO_MODULE_VERSION=#{settings['stratio_module_version']} STRATIO_MODULE_GITHUB_NAME=#{settings['stratio_module_github_name']} bash /vagrant/stratio_sandbox_common.sh"
+  config.vm.provision "shell", path: "stratio_sandbox_common.sh", args: "#{settings['stratio_env']} #{settings['stratio_module_name']} #{settings['stratio_module_version']} #{settings['stratio_module_github_name']}"
 
 end
--- stratio_sandbox_common.sh
+++ stratio_sandbox_common.sh
@@ -1,27 +1,25 @@
-
-export STRATIO_ENV="$(echo ${STRATIO_ENV:-PRE} | tr '[:lower:]' '[:upper:]')"
-export STRATIO_MODULE_GITHUB_NAME="${STRATIO_MODULE_GITHUB_NAME:-sandbox}"
+export STRATIO_ENV="$(echo ${1:-PRE} | tr '[:lower:]' '[:upper:]')"
+export STRATIO_MODULE=$2
+export STRATIO_MODULE_VERSION=$3
+export STRATIO_MODULE_GITHUB_NAME="${4:-sandbox}"
 
 case "$STRATIO_ENV" in
 	LOCAL) REPOSITORY="berilio.stratio.com/DEV/1.0.0"
 		;;
 	DEV) REPOSITORY="berilio.stratio.com/DEV/1.0.0"
 		;;
-	PRE) REPOSITORY="prerepository.stratio.com/TEST/1.0.0"
+	PRE) REPOSITORY="berilio.stratio.com/TEST/1.0.0"
 		;;
 	PRO) REPOSITORY="repository.stratio.com/stable"
 		;;
 	*) REPOSITORY="berilio.stratio.com/DEV/1.0.0"
 		;;
 esac		
-		
-REPOSITORY_URL="deb http://${REPOSITORY}/ubuntu/13.10/ binary/"
-
-export DEBIAN_FRONTEND=noninteractive
 
 #############
 ## FOLDERS ##
 #############
+mkdir -p /home/vagrant/module
 mkdir -p /home/vagrant/downloads
 
 ##################
@@ -30,27 +28,34 @@ mkdir -p /home/vagrant/downloads
 
 echo 'Loading repositories...'
 
-wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+yum -y -q install --nogpgcheck http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-add-apt-repository 'deb http://packages.elasticsearch.org/elasticsearch/1.3/debian stable main'
+# We add MongoDB repository
+cat >/etc/yum.repos.d/mongodb.repo <<EOF
+[mongodb]
+name=MongoDB Repository
+baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/
+gpgcheck=0
+enabled=1
+EOF
 
-echo "deb http://${REPOSITORY}/ubuntu/13.10/ binary/" | sudo tee -a /etc/apt/sources.list
-
-add-apt-repository ppa:webupd8team/java
-
-echo 'Updating repositories...'
-apt-get update -q -y
-
+# We add Stratio's repository
+cat >/etc/yum.repos.d/stratio.repo <<EOF
+[stratio]
+name=Stratio Package Repository
+baseurl=http://$REPOSITORY/RHEL/6.5
+gpgcheck=0
+enabled=1
+EOF
 
 ##########
 ## JAVA ##
 ##########
 
-echo 'Installing Java 7 oracle...'
-
+echo "Downloading Java 7 from Oracle..."
 curl -s -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.tar.gz > /tmp/jdk-7u71-linux-x64.tar.gz
+
+echo "Installing Java 7..."
 mkdir /usr/java && cd /usr/java
 tar -xzf /tmp/jdk-7u71-linux-x64.tar.gz
 chown -R root:root jdk1.7.0_71
@@ -60,22 +65,109 @@ update-alternatives --install /usr/bin/java java /usr/java/default/bin/java 1000
 update-alternatives --install /usr/bin/java java /usr/java/latest/bin/java 500
 rm -f /tmp/jdk-7u71-linux-x64.tar.gz
 
+cat >/etc/profile.d/java.sh <<EOF
+# The first existing directory is used for JAVA_HOME if needed.
+JVM_SEARCH_DIRS="/usr/java/default /usr/lib/jvm/jre-openjdk /usr/lib/jvm/jre /usr/lib/jvm/jre-1.7.* /usr/lib/jvm/java-1.7.*/jre"
+
+# If JAVA_HOME has not been set, try to determine it.
+if [ -z "\$JAVA_HOME" ]; then
+    # If java is in PATH, use a JAVA_HOME that corresponds to that. This is
+    # both consistent with how the upstream startup script works, and with
+    # the use of alternatives to set a system JVM (as is done on Debian and
+    # Red Hat derivatives).
+    java="\`/usr/bin/which java 2>/dev/null\`"
+    if [ -n "\$java" ]; then
+        java=\`readlink --canonicalize "\$java"\`
+        JAVA_HOME=\`dirname "\\\`dirname \\\$java\\\`"\`
+    else
+        # No JAVA_HOME set and no java found in PATH; search for a JVM.
+        for jdir in \$JVM_SEARCH_DIRS; do
+            if [ -x "\$jdir/bin/java" ]; then
+                JAVA_HOME="\$jdir"
+                break
+            fi
+        done
+        # if JAVA_HOME is still empty here, punt.
+    fi
+fi
+JAVA="\$JAVA_HOME/bin/java"
+export JAVA_HOME JAVA
+EOF
+
+###################
+## MISCELLANEOUS ##
+###################
+
+# TODO: MODIFY WITH CORPORATE MESSAGE
+cat >>/home/vagrant/.bash_profile <<EOF
+ipaddress_eth0=\$(ip -4 addr show dev eth0 | grep inet | sed -e 's/^.*inet \\(.*\\)\\/.*$/\\1/g')
+ipaddress_eth1=\$(ip -4 addr show dev eth1 | grep inet | sed -e 's/^.*inet \\(.*\\)\\/.*$/\\1/g')
+
+echo "Welcome to"
+echo "\$(figlet -f slant -c Stratio)"
+
+echo "Your IP addresses seem to be:"
+echo "   eth0: \$ipaddress_eth0"
+echo "   eth1: \$ipaddress_eth1"
+EOF
+
+echo "Adding IP to /etc/hosts..."
+ipaddress=$(ip -4 addr show dev eth0 | grep inet | sed -e 's/^.*inet \(.*\)\/.*$/\1/g')
+hostname=$(hostname --fqdn)
+echo "$ipaddress   $hostname" >> /etc/hosts
+# Adds ip address to /etc/hosts with every reboot, very convenient with DHCP network
+cat >/etc/init.d/ipaddress <<EOF
+#!/bin/bash
+#
+# /etc/init.d/ipaddress
+#
+# Just adds local ip address to /etc/hosts
+#
+# chkconfig: 345 40 60
+# description: Just adds local ip address to /etc/hosts
+
+ipaddress=\$(ip -4 addr show dev eth0 | grep inet | sed -e 's/^.*inet \\(.*\\)\\/.*$/\\1/g')
+hostname=\$(hostname --fqdn)
+
+grep -q \$hostname /etc/hosts
+ipexists=\$?
+
+if [ \$ipexists -eq 0 ]; then
+  sed -i "s/^.*\$hostname$/\$ipaddress   \$hostname/" /etc/hosts
+else
+  echo "\$ipaddress   \$hostname" >> /etc/hosts
+fi
+
+exit 0
+EOF
+chmod 755 /etc/init.d/ipaddress
+chkconfig --add ipaddress
+chkconfig ipaddress on
+
+echo "Installing additional packages..."
+yum -y -q --nogpgcheck install figlet git man grep
 
 ##############
 ## SERVICES ##
 ##############
 
-echo 'Installing common services...'
-apt-get install stratio-kafka stratio-zookeeper stratio-cassandra elasticsearch mongodb-org git -q -y --force-yes
-
+echo "Installing common services..."
+yum -y -q --nogpgcheck install stratio-kafka stratio-zookeeper stratio-cassandra stratio-elasticsearch mongodb-org
+chkconfig --add zookeeper
+chkconfig zookeeper off
+chkconfig --add kafka
+chkconfig kafka off
+chkconfig --add cassandra
+chkconfig cassandra off
+chkconfig --add elasticsearch
+chkconfig elasticsearch off
 
 ####################################
 ## DOWNLOAD & RUN SPECIFIC MODULE ##
 ####################################
 
-echo 'Installing common services...'
-mkdir -p /home/vagrant/module
+echo "Downloading module script..."
 DOWNLOAD_MODULE_SH_URL="https://raw.githubusercontent.com/Stratio/${STRATIO_MODULE_GITHUB_NAME}/master/sandbox/stratio_sandbox_module"
-curl $DOWNLOAD_MODULE_SH_URL > /home/vagrant/module/stratio_sandbox_module
+curl $DOWNLOAD_MODULE_SH_URL >/home/vagrant/module/stratio_sandbox_module
+echo "Executing module script..."
 bash /home/vagrant/module/stratio_sandbox_module
-
