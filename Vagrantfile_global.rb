b6c1ab2ef11e74bc6c486929b91a9acb475b2c56
mpenate <mpenate@stratio.com>
2015-09-03 12:44:13 +0200

    deleted:    LICENSE 	deleted:    README.md 	deleted:    Vagrantfile 	deleted:    VagrantfileBox 	new file:   Vagrantfile_example 	new file:   Vagrantfile_global.rb 	deleted:    stratio_sandbox_common.sh 	new file:   stratio_vagrant_script.sh 	modified:   vagrant_settings.yml 	new file:   vagrant_settings_l_example.yml

---
+++ Vagrantfile_global.rb
@@ -0,0 +1,81 @@
+require 'rexml/document'
+require 'yaml'
+include REXML
+
+class Vfg  
+  def run()    
+
+    open('vagrant_settings.yml', 'wb') do |file|            
+      file << open('http://172.19.1.35:8000/vagrant_settings.yml').read
+    end
+
+    open('stratio_vagrant_script.sh', 'wb') do |file|      
+      file << open('http://172.19.1.35:8000/stratio_vagrant_script.sh').read
+    end
+
+    if File.exist?("../banner")
+      file = File.open("../banner")
+      banner = ""
+      file.each { |line|
+        banner << line
+      }
+    end    
+
+    settings = YAML::load_file("vagrant_settings.yml")
+    if File.exist?("vagrant_settings_l.yml")       
+       settingslocal = YAML::load_file("vagrant_settings_l.yml")
+       ##Now we merge both hashes to combine all settings
+       settings = settings.merge(settingslocal)
+       puts "--- Local vagrant settings have been applied ---"
+    end    
+
+    file = File.new("../pom.xml")
+    doc = Document.new(file)
+    
+    stratio_module_fullname = doc.root.elements['name'].text
+    stratio_module_name = stratio_module_fullname.split.last.downcase
+    stratio_module_version = doc.root.elements['version'].text
+    stratio_hosts_config = ""
+    ip = ""
+
+    #Reading and transposing the hash to fetch ip-hostname duple
+    settings['private_hostnames'].each do |mod|      
+       mod.each do |x, y|
+        stratio_hosts_config << y.to_s << " " << x.to_s << ".box.stratio.com" << "\\n"
+      end      
+    end   
+
+    
+        
+    settings['private_hostnames'].each do |mod|      
+      if mod["#{stratio_module_name}"]!=nil 
+        ip = mod["#{stratio_module_name}"]     
+      break
+      end
+    end
+
+    Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+      #MODM
+      config.vm.box = settings['sandbox_base']
+      config.vm.hostname="#{stratio_module_name}.box.stratio.com"
+
+      # Create a private network, which allows host-only access to the machine      
+      config.vm.network "private_network", ip: ip      
+
+      config.vm.post_up_message = "Stratian: your stratio-#{stratio_module_fullname} Sandbox is now up & ready..."
+
+      config.vm.provider "virtualbox" do |vb|
+        # Boot with headless mode
+        vb.gui = false
+        vb.name = "stratio-#{stratio_module_name}-#{stratio_module_version}"
+
+        # Use VBoxManage to customize the VM. For example to change memory:
+        vb.memory = settings['sandbox_memory']
+        vb.cpus = settings['sandbox_cpus']
+        vb.customize ["modifyvm", :id, "--cpuexecutioncap", settings['sandbox_max_cpu_usage']]
+      end
+      
+      config.vm.provision "shell", path: "stratio_vagrant_script.sh", args: "#{stratio_module_name} '#{stratio_module_fullname}' #{stratio_module_version} '#{stratio_hosts_config}' '#{banner}'"      
+    end
+  end
+end
---
+++ stratio_vagrant_script.sh
@@ -0,0 +1,146 @@
+#################
+## ENVIRONMENT ##
+#################
+
+export STRATIO_MODULE_NAME=$1
+export STRATIO_MODULE_FULLNAME=$2
+export STRATIO_MODULE_VERSION=$3
+export STRATIO_MODULES_HOSTNAMES_IPS=$4
+export STRATIO_MODULE_BANNER=$5
+export WORK_MODULE_DIR=/home/vagrant/module
+export DOWNLOADS_DIR=/home/vagrant/downloads
+
+
+yum_install() {
+	yum install -y -q $@
+}
+
+add_repo() {
+##MODM
+wget "http://sodio.stratio.com/nexus/service/local/artifact/maven/content?r=releases-art&g=stratio&a=stratio-releases&c=noarch&p=rpm&v=LATEST" -O tmpfile.rpm
+yum -y localinstall tmpfile.rpm
+yum update 
+
+if [ STRATIO_MODULE == "streaming" ]; then
+    echo "Installing STREAMING packages"    
+    #yum -y install httpd stratio-streaming.noarch stratio-streaming-shell.noarch
+    yum -y install stratio-"${STRATIO_MODULE_NAME}"-"${STRATIO_MODULE_VERSION}" 
+    yum -y install stratio-"${STRATIO_MODULE_NAME}"-shell-"${STRATIO_MODULE_VERSION}" 
+else
+    echo "Installing your stratio module package..."
+    yum -y install stratio-"${STRATIO_MODULE_NAME}"-"${STRATIO_MODULE_VERSION}"
+fi
+}
+
+download() {
+	url="$1"
+	dest="$2"
+	shift 2
+	wget --no-check-certificate -q "$url" -O "$dest" $@
+}
+
+#############
+## FOLDERS ##
+#############
+
+mkdir -p "${WORK_MODULE_DIR}"
+mkdir -p "${DOWNLOADS_DIR}"
+
+##################
+## REPOSITORIES ##
+##################
+
+echo 'Loading repositories...'
+yum_install --nogpgcheck http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm 
+
+##ADDING STRATIO REPO
+echo 'Stratio inconming'
+wget "http://sodio.stratio.com/nexus/service/local/artifact/maven/content?r=releases-art&g=stratio&a=stratio-releases&c=noarch&p=rpm&v=LATEST" -O tmpfile.rpm
+yum -y localinstall tmpfile.rpm
+yum update 
+add_repo
+
+##########
+## JAVA ##
+##########
+
+echo "Checking java..."
+if type -p java; then
+    echo "OK. Java is already installed"
+else
+    echo "Downloading Java 7 from Oracle..."
+    curl -s -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.tar.gz > /tmp/jdk-7u71-linux-x64.tar.gz
+
+    echo "Installing Java 7..."
+    mkdir /usr/java && cd /usr/java
+    tar -xzf /tmp/jdk-7u71-linux-x64.tar.gz
+    chown -R root:root jdk1.7.0_71
+    ln -s jdk1.7.0_71 default
+    ln -s jdk1.7.0_71 latest
+    update-alternatives --install /usr/bin/java java /usr/java/default/bin/java 1000
+    update-alternatives --install /usr/bin/java java /usr/java/latest/bin/java 500
+    update-alternatives --install /usr/bin/jps jps /usr/java/default/bin/jps 1000
+    rm -f /tmp/jdk-7u71-linux-x64.tar.gz
+
+    echo "Setting up Java..."
+	
+    cat >/etc/profile.d/java.sh <<EOF
+# The first existing directory is used for JAVA_HOME if needed.
+JVM_SEARCH_DIRS="/usr/java/default /usr/lib/jvm/jre-openjdk /usr/lib/jvm/jre /usr/lib/jvm/jre-1.7.* /usr/lib/jvm/java-1.7.*/jre"
+# If JAVA_HOME has not been set, try to determine it.
+if [ -z "\$JAVA_HOME" ]; then
+    # If java is in PATH, use a JAVA_HOME that corresponds to that. This is
+    # both consistent with how the upstream startup script works, and with
+    # the use of alternatives to set a system JVM (as is done on Debian and
+    # Red Hat derivatives).
+    java="\`/usr/bin/which java 2>/dev/null\`"
+    if [ -n "\$java" ]; then
+        java=\`readlink --canonicalize "\$java"\`
+        JAVA_HOME=\`dirname "\\\`dirname \\\$java\\\`"\`
+    else
+        # No JAVA_HOME set and no java found in PATH; search for a JVM.
+        for jdir in \$JVM_SEARCH_DIRS; do
+            if [ -x "\$jdir/bin/java" ]; then
+                JAVA_HOME="\$jdir"
+                break
+            fi
+        done
+        # if JAVA_HOME is still empty here, punt.
+    fi
+fi
+JAVA="\$JAVA_HOME/bin/java"
+export JAVA_HOME JAVA
+EOF
+fi
+
+
+###################
+## MISCELLANEOUS ##
+###################
+
+##Figlet installation to show welcome message
+echo "Installing additional shell packages..."
+yum -y install figlet
+figlet -f slant -c ${STRATIO_MODULE_FULLNAME}>/home/welcome
+truncate -s -1 /home/welcome > /home/welcome
+echo "   ${STRATIO_MODULE_VERSION}" >> /home/welcome
+#Welcome with usefull network info 
+grep -qi stratio /home/vagrant/.bash_profile || cat >>/home/vagrant/.bash_profile <<EOF
+ipaddress_eth0=\$(ip -4 addr show dev eth0 | grep inet | sed -e 's/^.*inet \\(.*\\)\\/.*$/\\1/g')
+ipaddress_eth1=\$(ip -4 addr show dev eth1 | grep inet | sed -e 's/^.*inet \\(.*\\)\\/.*$/\\1/g')
+echo "Welcome to"
+echo "\$(cat /home/welcome)"
+echo "Your IP addresses seem to be:"
+echo "(nat/dhcp)       eth0: \$ipaddress_eth0   "
+echo "(hostonly/fixed) eth1: \$ipaddress_eth1   "
+echo ""
+echo "${STRATIO_MODULE_BANNER}"
+EOF
+
+## Add fixed ips to hosts
+echo "Adding IP to /etc/hosts..."
+echo -e $STRATIO_MODULES_HOSTNAMES_IPS >> /etc/hosts
+##Setting hostname 
+sed -i 's/^HOSTNAME=.*$/HOSTNAME='${STRATIO_MODULE_NAME}'.box.stratio.com/g' /etc/sysconfig/network
+##Delete net rules
+sed -i 's/^SUBSYSTEM/#SUBSYSTEM/g' /etc/udev/rules.d/70-persistent-net.rules 
