4cf91de328bc195b8926a1638195789903fd6e38
mpenate <mpenate@stratio.com>
2015-10-01 09:36:13 +0200

    Update Vagrantfile_global.rb
    Trying to get around authentication failures when box is packaged and exported

--- Vagrantfile_global.rb
+++ Vagrantfile_global.rb
@@ -61,6 +61,7 @@ class Vfg
       config.vm.box_url = settings['sandbox_base']
       config.vm.hostname="#{stratio_module_name}.box.stratio.com"
       config.vm.synced_folder '.', '/vagrant', disabled: true
+      config.ssh.insert_key = false
 
       # Create a private network, which allows host-only access to the machine  
       config.vm.network "public_network", bridge: "eth0"
